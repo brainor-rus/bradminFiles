5e2f345310f5362c9b13b6d4ced0173464179b1d
sumerk <sumerk.nya@gmail.com>
2018-10-09 17:11:44 +0300

    menu

--- src/Plugins/BrainorPay/Controllers/BrainorPayController.php
+++ src/Plugins/BrainorPay/Controllers/BrainorPayController.php
@@ -2,37 +2,32 @@
 
 namespace Bradmin\Plugins\BrainorPay\Controllers;
 
+use Bradmin\Controllers\BrAdminController;
+use Bradmin\Section;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\View;
 
-use Bradmin\Plugins\BrainorPay\Models\BrainorPayBank;
-use Bradmin\Plugins\BrainorPay\Helpers\GetData;
-
 class BrainorPayController extends Controller
 {
-    public function displayBanks()
+    public function showRouteRedirect(Section $section, $sectionName)
     {
+        $mainController = new BrAdminController;
 
-//        $banks = GetData::getPaymentData([
-//            'table' => 'brainor_pay_banks',
-//            'where' => function($query) {
-//                $query->where('bik', 1);
-//            }
-//        ]);
+        return $mainController->getDisplay($section, $sectionName);
+    }
 
-        $banks = new GetData;
+    public function createRouteRedirect(Section $section, $sectionName)
+    {
+        $mainController = new BrAdminController;
 
-        $banks->table('brainor_pay_banks');
+        return $mainController->getCreate($section, $sectionName);
+    }
 
-        $banks = $banks->get();
+    public function editRouteRedirect(Section $section, $sectionName, $id)
+    {
+        $mainController = new BrAdminController;
 
-        return response()->json([
-                'html' => View::make('brainorPay::admin.banks.display')->with(compact('banks'))->render(),
-                'meta' => [
-                    'title' => 'Банки'
-                ]
-            ]
-        );
+        return $mainController->getEdit($section, $sectionName, $id);
     }
 }
--- src/Plugins/BrainorPay/routes/web.php
+++ src/Plugins/BrainorPay/routes/web.php
@@ -4,3 +4,20 @@ Route::post('/'.config('bradmin.admin_url').'/pay/banks', [
     'as'   => 'bradmin.pay.banks.display',
     'uses' => 'Bradmin\Plugins\BrainorPay\Controllers\BrainorPayController@displayBanks',
 ]);
+
+// Редиректы /////////////////////////////////////////////////////////////////////////////
+
+Route::post('/'.config('bradmin.admin_url').'/pay/{sectionName}', [
+    'as'   => 'bradmin.pay.banks.display',
+    'uses' => 'Bradmin\Plugins\BrainorPay\Controllers\BrainorPayController@showRouteRedirect',
+]);
+
+Route::post('/'.config('bradmin.admin_url').'/pay/{sectionName}/create', [
+    'as'   => 'bradmin.pay.banks.display',
+    'uses' => 'Bradmin\Plugins\BrainorPay\Controllers\BrainorPayController@createRouteRedirect',
+]);
+
+Route::post('/'.config('bradmin.admin_url').'/pay/{sectionName}/{id}/edit', [
+    'as'   => 'bradmin.pay.banks.display',
+    'uses' => 'Bradmin\Plugins\BrainorPay\Controllers\BrainorPayController@editRouteRedirect',
+]);
