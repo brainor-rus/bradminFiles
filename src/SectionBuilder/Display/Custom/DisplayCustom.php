865620134c00196665e9fe891d29c10dd57577a0
Sergey Shlyakhov <sergey.schlyakhov@yandex.ru>
2018-11-22 16:34:51 +0400

    custom display section

---
+++ src/SectionBuilder/Display/Custom/DisplayCustom.php
@@ -0,0 +1,201 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Артем
+ * Date: 01.10.2018
+ * Time: 13:12
+ */
+
+namespace Bradmin\SectionBuilder\Display\Custom;
+
+use Bradmin\Section;
+use Bradmin\SectionBuilder\Meta\Meta;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\View;
+use BRHelper;
+
+class DisplayCustom
+{
+    private $pagination, $columns, $scopes, $meta, $view, $vars;
+
+
+    public function __construct($columns, $pagination, $view, $vars)
+    {
+        $this->setPagination($pagination);
+        $this->setColumns($columns);
+        $this->setView($view);
+        $this->setVars($vars);
+        $this->meta = new Meta;
+    }
+
+    public function render($modelPath, Section $firedSection, $pluginData = null)
+    {
+        $columns = $this->getColumns();
+        $relationData = null;
+
+        foreach ($columns as $column)
+        {
+            $exp = explode('.', $column->getName());
+            if(count($exp) > 1)
+            {
+                $relationData[] = implode(".", array_slice($exp, 0, -1));
+            }
+        }
+
+        $model = new $modelPath();
+
+        if(!empty($this->getScopes()))
+        {
+            foreach ($this->getScopes() as $scope)
+            {
+                $model = $model->{$scope}();
+            }
+        }
+
+        $data = $model->when(isset($relationData), function ($query) use ($relationData) {
+            $query->with($relationData);
+        })->paginate($this->getPagination());
+        $fields = array();
+
+        foreach ($data as $key => $row)
+        {
+            foreach ($columns as $column)
+            {
+                $names = explode('.', $column->getName());
+
+                $rowVal = $row;
+                foreach ($names as $name)
+                {
+                    if(!(is_array($rowVal) || $rowVal instanceof \Countable))
+                    {
+                        $rowVal = $rowVal->{$name} ?? null;
+                    } else
+                    {
+                        break;
+                    }
+                }
+
+                $fields[$key][$column->getName()] = $column->render($rowVal);
+            }
+            $fields[$key]['brRowId'] = $row->id;
+        }
+
+        if(isset($pluginData['redirectUrl']))
+        {
+            $rc = new \ReflectionClass($firedSection);
+            $pluginData['redirectUrl'] = strtr($pluginData['redirectUrl'], ['{sectionName}' => $rc->getShortName()]);
+        }
+
+
+        $response['data'] = $data;
+        $response['view'] = self::getView();
+
+        return $response;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getView()
+    {
+        return $this->view;
+    }
+
+    /**
+     * @param mixed $view
+     */
+    public function setView($view)
+    {
+        $this->view = $view;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getVars()
+    {
+        return $this->vars;
+    }
+
+    /**
+     * @param mixed $vars
+     */
+    public function setVars($vars)
+    {
+        $this->vars = $vars;
+        return $this;
+    }
+
+    /**
+     * @param mixed $pagination
+     * @return DisplayCustom
+     */
+    public function setPagination($pagination)
+    {
+        $this->pagination = $pagination;
+        return $this;
+    }
+
+    /**
+     * @param mixed $columns
+     * @return DisplayCustom
+     */
+    public function setColumns($columns)
+    {
+        $this->columns = $columns;
+        return $this;
+    }
+
+    /**
+     * @param mixed $scope
+     * @return DisplayCustom
+     */
+    public function setScopes($scopes)
+    {
+        $this->scopes = $scopes;
+        return $this;
+    }
+
+    /**
+     * @param mixed $meta
+     * @return DisplayCustom
+     */
+    public function setMeta($meta)
+    {
+        $this->meta = $meta;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getColumns()
+    {
+        return $this->columns;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPagination()
+    {
+        return $this->pagination;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getScopes()
+    {
+        return $this->scopes;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMeta()
+    {
+        return $this->meta;
+    }
+}
